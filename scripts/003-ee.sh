3aa3538b7e3f552544c29220adbab14627534403
KrahJohlito <krahjohlito@outlook.com>
2024-11-12 12:20:16 +1030

    point build scripts to reverted repos

--- scripts/003-ee.sh
+++ scripts/003-ee.sh
@@ -2,11 +2,11 @@
 # 003-ee.sh by Francisco Javier Trujillo Mata (fjtrujy@gmail.com)
 
 ## Download the source code.
-REPO_URL="https://github.com/ps2dev/ps2toolchain-ee"
+REPO_URL="https://github.com/KrahJohlito/ps2toolchain-ee"
 REPO_FOLDER="ps2toolchain-ee"
 
 # Checking if a specific TAG has been selected, it is passed using parameter $1
-[  -z "$1" ] && REPO_REFERENCE="main" || REPO_REFERENCE=$1
+[  -z "$1" ] && REPO_REFERENCE="ee-2023" || REPO_REFERENCE=$1
 echo "Using repo reference $REPO_REFERENCE"
 
 if test ! -d "$REPO_FOLDER"; then
